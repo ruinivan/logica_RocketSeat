5c5c73956a272cb6ec033613492df4a2ff9e3d4e
Victor Ruinivan <victorruinivanvg@gmail.com>
2025-08-02 23:48:56 -0300

    .

--- primeiros-programas/oraculo-programacao.py
+++ primeiros-programas/oraculo-programacao.py
@@ -1,6 +1,6 @@
-print('Olá, eu sou sua assistente Pytrina');
-print('O que eu posso te ajudar hoje?');
-comando = input('Digite um comando: ');
+print('Olá, eu sou o Oraculo da Porgramação')
+print('O que posso te ensinar hoje?')
+comando = input('Digite um comando: ')
 
 match comando:
     case 'oi' | 'ola':
